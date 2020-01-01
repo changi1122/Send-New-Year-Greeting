0798cf83ff1161547080edb90763b774f7ddca0d
changi1122 <changi112242@gmail.com>
2020-01-01 20:51:19 +0900

    [메시지] 프로젝트 첫 커밋, 메시지 클래스 구현

---
+++ SendNewYearGreeting/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using SendNewYearGreeting.Classes;
+
+namespace SendNewYearGreeting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
