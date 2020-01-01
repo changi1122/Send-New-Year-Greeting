8574d005377888e6fb362494602a31e7bc2bd770
changi1122 <changi112242@gmail.com>
2020-01-02 08:32:38 +0900

    [전화번호] Address 클래스 구현

--- SendNewYearGreeting/Program.cs
+++ SendNewYearGreeting/Program.cs
@@ -7,7 +7,26 @@ namespace SendNewYearGreeting
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Send New Year Greeting  새해 인사 보내기 프로그램");
+            Console.WriteLine("Powered by Twilio");
+
+            PrintMenu();
+
             Console.WriteLine("Hello World!");
+        }
+
+        static void PrintMenu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("> mess          : 전송할 메시지 설정");
+            Console.WriteLine("> list          : 메시지를 전송할 전화번호 출력");
+            Console.WriteLine("> set list      : 메시지를 전송할 전화번호 설정");
+            Console.WriteLine("> ac            : Twilio 계정 정보 출력");
+            Console.WriteLine("> set ac        : Twilio 계정 정보 설정");
+            Console.WriteLine("> send          : 메시지 전송");
+            Console.WriteLine("> help          : 도움말 출력");
+            Console.WriteLine("> (명령) --help : 명령별 도움말 출력");
+            Console.WriteLine();
         }
     }
 }
