4a65c5159e1534d259efc70860d1c01d8b6d064d
changi1122 <changi112242@gmail.com>
2020-01-04 00:05:59 +0900

    [명령] 명령어 입력 받기, 각각 메시지 보내기, 도움말

--- SendNewYearGreeting/Classes/Content.cs
+++ SendNewYearGreeting/Classes/Content.cs
@@ -22,6 +22,7 @@ namespace SendNewYearGreeting.Classes
                 StreamReader sr = new StreamReader(fs, Encoding.UTF8);
                 string body = sr.ReadToEnd();
                 sr.Close();
+                fs.Close();
                 return body;
             }
 
@@ -36,12 +37,49 @@ namespace SendNewYearGreeting.Classes
                 StreamWriter sw = new StreamWriter(fs, Encoding.UTF8);
                 sw.Write(value);
                 sw.Close();
+                fs.Close();
 
                 //Swap new file for account file
                 if (File.Exists("content.txt"))
                     File.Delete("content.txt");
                 File.Move("_content.txt", "content.txt");
             }
+        }
+
+        public static string GetSubstitutedBody(string name)
+        {
+            string body = Body;
+            
+            //Substitution
+            if (body.Contains("<name>"))
+            {
+                body = body.Replace("<name>", name);
+            }
+            if (body.Contains("<random>"))
+            {
+                body = body.Replace("<random>", GetGreetingStrings());
+            }
+
+            return body;
+        }
+
+        private static string GetGreetingStrings()
+        {
+            string[] GREETINGSTRINGS = {
+                "새해에는 행운과 평안이 가듯하기를 기원합니다",
+                "새해에는 소망하는 일 모두 이루세요!",
+                "가족 모두 행복한 한 해 보내세요!",
+                "새해 복 많이 받으시고 올 한 해도 건강해지길 바랍니다.",
+                "새해에도 웃음 가득한 한 해 되세요.",
+                "새해에도 늘 행복하고 건강하세요",
+                "새해에도 좋은 일만 가득하기를 기원합니다.",
+                "반짝반짝 빛나는 한 해가 되기를 기원합니다."
+            };
+
+            Random r = new Random();
+            string gs = GREETINGSTRINGS[r.Next(0, GREETINGSTRINGS.Length)];
+
+            return gs;
         }
 
         public static void SetBody()
--- SendNewYearGreeting/Program.cs
+++ SendNewYearGreeting/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using SendNewYearGreeting.Classes;
 
 namespace SendNewYearGreeting
@@ -12,7 +13,16 @@ namespace SendNewYearGreeting
 
             PrintMenu();
 
-            Console.WriteLine("Hello World!");
+            while(true)
+            {
+                Console.WriteLine();
+                Console.Write("> ");
+                string command = Console.ReadLine();
+
+                //Execute command
+                Command(command);
+            }
+
         }
 
         static void PrintMenu()
@@ -20,13 +30,154 @@ namespace SendNewYearGreeting
             Console.WriteLine();
             Console.WriteLine("> mess          : 전송할 메시지 설정");
             Console.WriteLine("> list          : 메시지를 전송할 전화번호 출력");
-            Console.WriteLine("> set list      : 메시지를 전송할 전화번호 설정");
+            Console.WriteLine("> setlist       : 메시지를 전송할 전화번호 설정");
             Console.WriteLine("> ac            : Twilio 계정 정보 출력");
-            Console.WriteLine("> set ac        : Twilio 계정 정보 설정");
+            Console.WriteLine("> setac         : Twilio 계정 정보 설정");
             Console.WriteLine("> send          : 메시지 전송");
             Console.WriteLine("> help          : 도움말 출력");
             Console.WriteLine("> (명령) --help : 명령별 도움말 출력");
             Console.WriteLine();
         }
+
+        static void Command(string command)
+        {
+            string cmd = "", option = "";
+
+            //Command Split
+            string[] words = command.Split();
+            if (0 < words.Length)
+                cmd = words[0];
+            if (1 < words.Length)
+                option = words[1];
+
+            //Execute command
+            if (cmd == "mess")
+            {
+                if (option == "--help")
+                {
+                    //print mess menual
+                    Console.WriteLine("\n[mess (message)]");
+                    Console.WriteLine("전송할 메시지 내용을 설정합니다. \"<name>\"을 적으면 이름으로, \"<random>\"을 적으면 새해 인사말로 자동 치환됩니다.");
+                }
+                else
+                    Content.SetBody();
+            }
+            else if (cmd == "list")
+            {
+                if (option == "--help")
+                {
+                    //print list menual
+                    Console.WriteLine("\n[list]");
+                    Console.WriteLine("메시지를 전송할 전화번호 목록을 출력합니다.");
+                }
+                else
+                {
+                    if (!System.IO.File.Exists("address.txt"))
+                    {
+                        Console.WriteLine("전화번호가 존재하지 않습니다.");
+                        return;
+                    }
+
+                    List<Address> addresses = AddressData.LoadAddress();
+
+                    //Print whole list
+                    Console.WriteLine();
+                    for (int i = 0; i < addresses.Count; i++)
+                        Console.WriteLine($"{i + 1}: {addresses[i].name}\t\t{addresses[i].tel}");
+                }
+            }
+            else if (cmd == "setlist")
+            {
+                if (option == "--help")
+                {
+                    //print setlist menual
+                    Console.WriteLine("\n[setlist]");
+                    Console.WriteLine("메시지를 전송할 전화번호 목록을 설정합니다. add를 입력해 뒤에 추가하거나, mod를 입력해 특정 번호를 수정하고, del을 입력해 목록을 삭제하고 다시 입력할 수 있습니다.");
+                }
+                else
+                    AddressSet.SetAddress();
+            }
+            else if (cmd == "ac")
+            {
+                if (option == "--help")
+                {
+                    //print ac menual
+                    Console.WriteLine("\n[ac (account)]");
+                    Console.WriteLine("Twilio 계정 정보를 출력합니다.");
+                }
+                else
+                    Message.PrintAccount();
+            }
+            else if (cmd == "setac")
+            {
+                if (option == "--help")
+                {
+                    //print setac menual
+                    Console.WriteLine("\n[setac (set account)]");
+                    Console.WriteLine("Twilio 계정 정보를 설정합니다. 메시지 전송을 위해 Account Sid, Auth Token이 필요합니다.");
+                }
+                else
+                    Message.SetAccount();
+            }
+            else if (cmd == "send")
+            {
+                if (option == "--help")
+                {
+                    //print send menual
+                    Console.WriteLine("\n[send]");
+                    Console.WriteLine("전화번호 목록의 각각에 메시지를 보냅니다. 메시지를 보내기 전, 내용을 확인하고 수정할 수 있습니다.");
+                }
+                else
+                {
+                    Message.SendMessageForeachList();
+                }
+            }
+            else if (cmd == "help")
+            {
+                //print help
+                Console.WriteLine("\n[mess (message)]");
+                Console.WriteLine("전송할 메시지 내용을 설정합니다. \"<name>\"을 적으면 이름으로, \"<random>\"을 적으면 새해 인사말로 자동 치환됩니다.");
+
+                Console.WriteLine("\n[list]");
+                Console.WriteLine("메시지를 전송할 전화번호 목록을 출력합니다.");
+
+                Console.WriteLine("\n[setlist]");
+                Console.WriteLine("메시지를 전송할 전화번호 목록을 설정합니다. add를 입력해 뒤에 추가하거나, mod를 입력해 특정 번호를 수정하고, del을 입력해 목록을 삭제하고 다시 입력할 수 있습니다.");
+
+                Console.WriteLine("\n[ac (account)]");
+                Console.WriteLine("Twilio 계정 정보를 출력합니다.");
+
+                Console.WriteLine("\n[setac (set account)]");
+                Console.WriteLine("Twilio 계정 정보를 설정합니다. 메시지 전송을 위해 Account Sid, Auth Token이 필요합니다.");
+
+                Console.WriteLine("\n[send]");
+                Console.WriteLine("전화번호 목록의 각각에 메시지를 보냅니다. 메시지를 보내기 전, 내용을 확인하고 수정할 수 있습니다.");
+
+                Console.WriteLine("\n[clear]");
+                Console.WriteLine("콘솔의 텍스트를 모두 지웁니다.");
+            }
+            else if (cmd == "clear")
+            {
+                if (option == "--help")
+                {
+                    //print clear menual
+                    Console.WriteLine("\n[clear]");
+                    Console.WriteLine("콘솔의 텍스트를 모두 지웁니다.");
+                }
+                else
+                {
+                    Console.Clear();
+                }
+            }
+            else
+            {
+                Console.WriteLine($"\"{cmd}\"는 알 수 없는 명령입니다.");
+            }
+        }
+
+
+
+
+
     }
 }
